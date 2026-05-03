ce12fdfafbf827e7cf4aaeaa09460d7b7c8b0a27
TawhidRahman011209 <lovelyshamima28@gmail.com>
2026-05-04 01:53:14 +0600

    some improvement done

---
+++ README.md
@@ -0,0 +1,27 @@
+│
+├── server/                # Backend (Express)
+│   ├── config/
+│   │   ├── db.js
+│   ├── controllers/
+│   │   ├── authController.js
+│   │   ├── productController.js
+│   │   ├── orderController.js
+│   │   ├── userController.js
+│   ├── middleware/
+│   │   ├── authMiddleware.js
+│   │   ├── roleMiddleware.js
+│   ├── models/
+│   │   ├── User.js
+│   │   ├── Product.js
+│   │   ├── Order.js
+│   ├── routes/
+│   │   ├── authRoutes.js
+│   │   ├── productRoutes.js
+│   │   ├── orderRoutes.js
+│   │   ├── userRoutes.js
+│   ├── utils/
+│   │   ├── generateToken.js
+│   ├── .env
+│   ├── index.js
+│   ├── package.json
+
--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const orderRoutes = require("./routes/orderRoutes");
 
 const userRoutes = require("./routes/userRoutes");
 
+const Product = require("./models/Product");
+
 const app = express();
 
 connectDB();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
@@ -40,6 +40,54 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
 app.use("/api/users", userRoutes);
+
+const insertSampleProducts = async () => {
+  try {
+    const count = await Product.countDocuments();
+
+    if (count === 0) {
+      await Product.insertMany([
+        {
+          name: "Cotton T-Shirt",
+          category: "T-Shirt",
+          price: 12,
+          quantity: 100,
+          moq: 10,
+          images: [
+            "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=1000&auto=format&fit=crop",
+          ],
+          description: "High quality cotton t-shirt",
+          paymentOption: "Cash",
+          demoVideo: "",
+          showOnHome: true,
+          createdBy: "manager@gmail.com",
+        },
+
+        {
+          name: "Blue Hoodie",
+          category: "Hoodie",
+          price: 30,
+          quantity: 50,
+          moq: 5,
+          images: [
+            "https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=1000&auto=format&fit=crop",
+          ],
+          description: "Warm blue hoodie",
+          paymentOption: "Card",
+          demoVideo: "",
+          showOnHome: true,
+          createdBy: "manager@gmail.com",
+        },
+      ]);
+
+      console.log("Sample products inserted");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+insertSampleProducts();
 
 const PORT = process.env.PORT || 5000;
 
