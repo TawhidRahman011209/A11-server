2c2096c2b31481068681373ad120586929ceefe8
TawhidRahman011209 <lovelyshamima28@gmail.com>
2026-05-05 02:28:24 +0600

    fix problem during deployment

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,20 +1,7 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = {
-  type: process.env.FIREBASE_type,
-  project_id: process.env.FIREBASE_project_id,
-  private_key_id: process.env.FIREBASE_private_key_id,
-  private_key: process.env.FIREBASE_private_key.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_client_email,
-  client_id: process.env.FIREBASE_client_id,
-  auth_uri: process.env.FIREBASE_auth_uri,
-  token_uri: process.env.FIREBASE_token_uri,
-  auth_provider_x509_cert_url:
-    process.env.FIREBASE_auth_provider_x509_cert_url,
-  client_x509_cert_url:
-    process.env.FIREBASE_client_x509_cert_url,
-  universe_domain: process.env.FIREBASE_universe_domain,
-};
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
