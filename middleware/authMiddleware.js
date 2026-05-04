0aa596a710bad0d2fb8f3a2fbfccf9797ee51472
TawhidRahman011209 <lovelyshamima28@gmail.com>
2026-05-04 13:23:20 +0600

    role selecting fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,30 +1,22 @@
-const jwt = require("jsonwebtoken");
+const admin = require("../firebaseAdmin");
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   try {
-    const authHeader =
-      req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-      return res.status(401).json({
-        message: "Unauthorized",
-      });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
     const token = authHeader.split(" ")[1];
 
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    );
+    const decoded = await admin.auth().verifyIdToken(token);
 
     req.user = decoded;
 
     next();
   } catch (error) {
-    res.status(401).json({
-      message: "Unauthorized",
-    });
+    return res.status(401).json({ message: "Unauthorized" });
   }
 };
 
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,16 +1,16 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const generateToken = (user) => {
-  return jwt.sign(
-    {
-      email: user.email,
-      role: user.role,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "7d",
-    }
-  );
-};
+// const generateToken = (user) => {
+//   return jwt.sign(
+//     {
+//       email: user.email,
+//       role: user.role,
+//     },
+//     process.env.JWT_SECRET,
+//     {
+//       expiresIn: "7d",
+//     }
+//   );
+// };
 
-module.exports = generateToken;
+// module.exports = generateToken;
