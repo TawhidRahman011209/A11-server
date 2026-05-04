0835bbe7a12b92369fb02c409d5eecb72ef23b81
TawhidRahman011209 <lovelyshamima28@gmail.com>
2026-05-05 02:57:07 +0600

    try to deploy

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
+      "https://a11-client.netlify.app",
     ],
     credentials: true,
   })
